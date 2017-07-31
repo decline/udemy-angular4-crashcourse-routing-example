57c9102f353ad6b4cbdf78dfdb50ddba515f27fd
dominik.huppmann <dominik.huppmann@gmail.com>
2017-07-31 22:46:55 +0200

    add "combineLatest" to subscribe to multiple Observables / use "switchMap" to return an "any" Type instead of an Observable<any> type

--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -12,6 +12,11 @@ export class GithubProfileComponent implements OnInit {
     }
 
     ngOnInit() {
+        // this can be used, if the parametesr will never change for the same component
+        let id = this.route.snapshot.paramMap.get('id');
+        console.log(id);
+
+        // but we should use this, when we the route parameters change, but the component is not re-initialized!
         this.route.paramMap.subscribe(params => {
             let id = +params.get('id');
             console.log(id);
